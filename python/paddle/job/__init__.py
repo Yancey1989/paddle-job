8edd69062ad9167a761a5c31a19cf78fd0e3d0af
Yancey1989 <yancey1989@gmail.com>
2017-05-04 16:18:18 +0800

    fix import uilts.dist_train error

--- python/paddle/job/__init__.py
+++ python/paddle/job/__init__.py
@@ -1,6 +1,6 @@
-import utils.dist_train
 from job_manager import JobManager
 from paddle_job import PaddleJob
 from ceph_volume import CephVolume
+from utils import dist_train
 
 __all__ = ["CephVolume", "JobManager", "PaddleJob", "dist_train"]
